311e082fbb2e9e112fb5f0ecead1a9c4ca64a0c0
Jamie Cao <jamiecao402@gmail.com>
2022-05-02 22:06:11 -0400

    Removed search button and made search by enter only

--- sleep-deprived-gps/src/SearchBox.jsx
+++ sleep-deprived-gps/src/SearchBox.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import L from 'leaflet';
 import { TextField } from "@mui/material";
 import { Button } from "@mui/material";
-import { List, ListItem, ListItemIcon, ListItemText } from "@mui/material";
+import { List, ListItem, ListItemIcon, ListItemText, Divider } from "@mui/material";
 
 const NOMINATIM_BASE_URL = "https://nominatim.openstreetmap.org/search?";
 const params = {
@@ -19,41 +19,81 @@ function SearchBox(props) {
 
     return (
         <div className="searchbox-container">
-            <div style={{ display: "flex" }}>
-                <div style={{ flex: 1 }}>
-                    <TextField style={{ width: "100%" }} 
-                        value={searchText}
-                        onChange={(e) => {
-                            setSearchText(e.target.value);
-                        }}
-                    />
-                </div>
-                <div style={{ display: "flex",  alignItems: "center", padding: "0px 10px" }}>
-                    <Button variant="contained" color="primary" onClick={() => {
-                        const params = {
-                            q: searchText,
-                            format: 'json',
-                            addressdetails: 1,
-                            polygon_geojson: 0,
-                        };
-                        const queryString = new URLSearchParams(params).toString();
-                        const requestOptions = {
-                            method: "GET",
-                            redirect: "follow",
-                        };
-                        fetch(`${NOMINATIM_BASE_URL}${queryString}`, requestOptions)
-                            .then((response) => response.text())
-                            .then((result) => {
-                                console.log(JSON.parse(result));
-                                setListPlace(JSON.parse(result));
-                            })
-                            .catch((err) => console.log("err: ", err));
-                    }}>
-                        Search
-                    </Button>
-                </div>
+            <div style={{ padding: "8px" }}>
+                <TextField
+                    style={{ width: "100%" }}
+                    id="outlined-basic"
+                    label="Start Location"
+                    variants="outlined"
+                    size="small"
+                    color="primary" focused
+                    value={searchText}
+                    onChange={(e) => {
+                        setSearchText(e.target.value);
+                    }}
+                    onKeyDown={(e) => {
+                        if(e.keyCode == 13) {
+                            e.preventDefault();
+                            const params = {
+                                q: searchText,
+                                format: 'json',
+                                addressdetails: 1,
+                                polygon_geojson: 0,
+                            };
+                            const queryString = new URLSearchParams(params).toString();
+                            const requestOptions = {
+                                method: "GET",
+                                redirect: "follow",
+                            };
+                            fetch(`${NOMINATIM_BASE_URL}${queryString}`, requestOptions)
+                                .then((response) => response.text())
+                                .then((result) => {
+                                    console.log(JSON.parse(result));
+                                    setListPlace(JSON.parse(result));
+                                })
+                                .catch((err) => console.log("err: ", err));
+                        }
+                    }}
+                />
             </div>
-            <List component="nav" aria-label="main mailbox folders" >
+            <div style={{ padding: "8px" }}>
+                <TextField
+                    style={{ width: "100%" }}
+                    id="outlined-basic"
+                    label="End Location"
+                    variants="outlined"
+                    size="small"
+                    color="secondary" focused
+                    value={searchText}
+                    onChange={(e) => {
+                        setSearchText(e.target.value);
+                    }}
+                    onKeyDown={(e) => {
+                        if(e.keyCode == 13) {
+                            e.preventDefault();
+                            const params = {
+                                q: searchText,
+                                format: 'json',
+                                addressdetails: 1,
+                                polygon_geojson: 0,
+                            };
+                            const queryString = new URLSearchParams(params).toString();
+                            const requestOptions = {
+                                method: "GET",
+                                redirect: "follow",
+                            };
+                            fetch(`${NOMINATIM_BASE_URL}${queryString}`, requestOptions)
+                                .then((response) => response.text())
+                                .then((result) => {
+                                    console.log(JSON.parse(result));
+                                    setListPlace(JSON.parse(result));
+                                })
+                                .catch((err) => console.log("err: ", err));
+                        }
+                    }}
+                />
+            </div>
+            <List component="nav" aria-label="main mailbox folders">
                 {listPlace.map((item) => {
                     return (
                         <div key={item?.osm_id}>
@@ -65,6 +105,7 @@ function SearchBox(props) {
                                 </ListItemIcon>
                                 <ListItemText primary={item?.display_name} />
                             </ListItem>
+                            <Divider />
                         </div>
                     );
                 })}
